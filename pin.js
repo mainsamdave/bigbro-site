3ab4853d9404b71a1c2157a5e21365946fb9996b
mainsamdave <johncrane239@gmail.com>
2026-03-28 14:22:32 +0000

    Create pin.js

---
+++ pin.js
@@ -0,0 +1,17 @@
+const pinataSDK = require('pinata-sdk');
+const path = require('path');
+
+const pinata = pinataSDK(
+  process.env.PINATA_API_KEY,
+  process.env.PINATA_API_SECRET
+);
+
+const folderPath = path.join(process.cwd(), 'bigbro');
+console.log('Pinning folder:', folderPath);
+
+pinata.pinFromFS(folderPath, { pinataMetadata: { name: 'bigbro-site' } })
+  .then(result => console.log('Pinned! CID:', result.IpfsHash))
+  .catch(err => {
+    console.error('Pin failed:', err);
+    process.exit(1);
+  });
